cf08dce19a70c491630ff91277ddd99cb8ffa33e
Suresh Ghimire <sghimire38288@ucumberlands.edu>
2025-10-24 20:47:13 -0700

    added spring boot project

---
+++ TodoListApp-Java/src/main/java/dev/sghimire/TodoListApp_Java/TodoListAppJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.sghimire.TodoListApp_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListAppJavaApplication.class, args);
+	}
+
+}
---
+++ TodoListApp-Java/src/test/java/dev/sghimire/TodoListApp_Java/TodoListAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sghimire.TodoListApp_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
